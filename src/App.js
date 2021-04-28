5f406151550752fd48c75cbdf356ebe7c3920b79
Jot singh <jot.chawla.54.js@gmail.com>
2021-04-28 08:57:29 +0530

    created React Project And Added React Router

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Main } from "./components/MainComponent";
+import { HashRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <HashRouter>
+      <div className="App">
+        <Main />
+      </div>
+    </HashRouter>
+  );
+}
+
+export default App;
